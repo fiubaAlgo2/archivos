80f38bf4d6136c4a37cd1889c7a0527df35a642b
cpistillo <cpistillo@fi.uba.ar>
2021-04-15 10:47:04 -0300

    read from file

--- main.cpp
+++ main.cpp
@@ -1,29 +1,16 @@
 #include <iostream>
 #include <fstream>
-#include <unistd.h>
 
-std::string get_current_dir() {
-    char buff[FILENAME_MAX];
-    getcwd( buff, FILENAME_MAX );
-    std::string current_working_dir(buff);
-    return current_working_dir;
-}
+int main() {
 
-int main()
-{
-    std::ifstream archivo;
-    archivo.open("datos.txt");
+    std::ifstream file;
+    file.open("../datos.txt");
+    std::string word;
 
-    if(!archivo)
-    {
-        std::cout << "No se pudo abrir el archivo" << std::endl;
-        std::cout << get_current_dir() << std::endl;
+    while(file >> word){
+        std::cout << word << " ";
     }
-
-    else {
-        std::cout << "Archivo abierto correctamente" << std::endl;
-    }
-
 
     return 0;
 }
+
