3302458ba039e295515cb7de4146f07877b0f947
cpistillo <cpistillo@fi.uba.ar>
2021-04-15 10:40:57 -0300

    Open file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <unistd.h>
+
+std::string get_current_dir() {
+    char buff[FILENAME_MAX];
+    getcwd( buff, FILENAME_MAX );
+    std::string current_working_dir(buff);
+    return current_working_dir;
+}
 
 int main()
 {
-    std::cout << "Hello world" << std::endl;
+    std::ifstream archivo;
+    archivo.open("datos.txt");
+
+    if(!archivo)
+    {
+        std::cout << "No se pudo abrir el archivo" << std::endl;
+        std::cout << get_current_dir() << std::endl;
+    }
+
+    else {
+        std::cout << "Archivo abierto correctamente" << std::endl;
+    }
+
+
     return 0;
 }
